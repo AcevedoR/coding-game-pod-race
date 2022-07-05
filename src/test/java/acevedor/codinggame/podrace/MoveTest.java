fa588ed40d05e7759feaea058dd59c2ac719416b
acevedor <roman.aceved62@gmail.com>
2022-07-05 15:09:44 +0200

    forgot generation test

---
+++ src/test/java/acevedor/codinggame/podrace/MoveTest.java
@@ -0,0 +1,25 @@
+package acevedor.codinggame.podrace;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MoveTest {
+
+    @Test
+    public void generateRandom_ok(){
+        Move move = new Move(-1, -1);
+        int amplitude = 18;
+        int speerR = 7;
+        for(int s = 0; s < speerR; s++) {
+            for (int a = 0; a < amplitude; a++) {
+                move.mutate(amplitude, speerR, a, s);
+                assertThat(move.angle).isBetween(-18.0, 18.0);
+                assertThat(move.thrust).isBetween(0, GameState.MAX_THRUST);
+            }
+        }
+    }
+}
